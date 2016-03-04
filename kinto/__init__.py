016df8f10ef32324a55c25a8bb3f3449f7e401d4
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-03-04 09:29:18 +0100

    As bool not necessary here.

--- kinto/__init__.py
+++ kinto/__init__.py
@@ -54,9 +54,7 @@ def main(global_config, config=None, **settings):
     config.registry.public_settings.add('cliquet.batch_max_requests')
 
     # Expose capability
-    schema_enabled = asbool(
-        settings['experimental_collection_schema_validation']
-    )
+    schema_enabled = settings['experimental_collection_schema_validation']
     if schema_enabled:
         config.add_api_capability(
             "schema",
